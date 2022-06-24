57e40ac7e0ab3edf96378d74e43f563ef74ec41f
Lindsey Bell <lindsey.bell@webdevstudios.com>
2022-06-24 15:57:52 -0400

    Added front template; added page-hero pattern; other stuff

---
+++ patterns/page-hero.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Page Hero
+ * Slug: petrichor/page-hero
+ * Block types: core/cover
+ * Categories: headers, hero
+ */
+?>
+
+<!-- wp:cover {"url":"https://quasi-org.local/wp-content/uploads/2008/06/windmill.jpg","id":767,"hasParallax":true,"dimRatio":50,"overlayColor":"primary"} -->
+<div class="wp-block-cover has-parallax" style="background-image:url(https://quasi-org.local/wp-content/uploads/2008/06/windmill.jpg)">
+	<span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span>
+	<div class="wp-block-cover__inner-container"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"secondary","fontSize":"large"} -->
+		<h2 class="has-secondary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:700">
+			<strong>Petrichor Theme</strong>
+		</h2>
+<!-- /wp:heading -->
+	</div>
+</div>
+<!-- /wp:cover -->
